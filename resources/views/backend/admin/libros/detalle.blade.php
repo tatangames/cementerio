c057187257ef9ceb957053590ffbd4cc80f72912
riveroo98 <angelriverar2403@gmail.com>
2025-02-01 12:32:37 -0600

    Vista de inicio

--- resources/views/backend/admin/libros/detalle.blade.php
+++ resources/views/backend/admin/libros/detalle.blade.php
@@ -72,12 +72,12 @@
             <div class="fila">
                 <div class="form-group-peque d-flex align-items-center">
                     <label class="me-2">Libro:</label>
-                    <input type="text" maxlength="50" autocomplete="off" class="form-control" name="libro[]" style="width: auto;">
+                    <input type="text" maxlength="50" autocomplete="off" class="form-control" id="libro" style="width: auto;">
                 </div>
 
                 <div class="form-group-peque d-flex align-items-center">
                     <label>Nicho:</label>
-                    <input type="text" maxlength="50" autocomplete="off" class="form-control" name="nicho[]">
+                    <input type="text" maxlength="50" autocomplete="off" class="form-control" id="nicho">
                 </div>
             </div>
 
@@ -86,103 +86,90 @@
 
                 <div class="form-group d-flex align-items-center">
                     <label>Nombre Primer fallecido</label>
-                    <input type="text" maxlength="50" autocomplete="off" class="form-control" name="nombre[]">
+                    <input type="text" maxlength="50" autocomplete="off" class="form-control" id="nombreeditar">
                 </div>
                 <div class="form-group d-flex align-items-center">
                     <label>Fecha de fallecimiento:</label>
-                    <input type="date" maxlength="50" autocomplete="off" class="form-control" name="fechafallecimiento[]">
+                    <input type="date" maxlength="50" autocomplete="off" class="form-control" id="fechafallecimiento">
                 </div>
                 <div class="form-group d-flex align-items-center">
                     <label>Fecha de exhumacion:</label>
-                    <input type="date" maxlength="50" autocomplete="off" class="form-control" name="fechaexhumacion[]">
+                    <input type="date" maxlength="50" autocomplete="off" class="form-control" id="fechaexhumacion">
                 </div>
 
-
             </div>
-            <!-- Campos adicionales -->
-            <div id="campos-adicionales"></div>
-
-            <!-- Botón para agregar más fallecidos -->
-{{--            <button type="button" class="btn btn-primary" onclick="agregarFallecido()">--}}
-{{--                Agregar fallecido--}}
-{{--            </button>--}}
 
             <!-- Fila 2: Fecha de exhumación y Fecha de vencimiento -->
             <div class="fila">
 
                 <div class="form-group">
                     <label>Fecha de vencimiento:</label>
-                    <input type="date" maxlength="8" autocomplete="off" class="form-control" name="fechavencimiento[]">
+                    <input type="date" maxlength="8" autocomplete="off" class="form-control" id="fechavencimiento">
                 </div>
                 <div class="form-group">
                     <label>Periodo de mora:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="periodo_en_mora[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="periodo_en_mora">
                 </div>
                 <div class="form-group">
                     <label>Personas en mora:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="persona_en_mora[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="persona_en_mora">
                 </div>
                 <div class="form-group">
                     <label>Cancelacion sin el 5% x periodo:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="cancelacion_sin[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="cancelacion_sin">
                 </div>
                 <div class="form-group">
                     <label>Prox fecha de vencimiento:</label>
-                    <input type="date" maxlength="8" autocomplete="off" class="form-control" name="proxfecha[]">
+                    <input type="date" maxlength="8" autocomplete="off" class="form-control" id="proxfecha">
                 </div>
             </div>
 
             <!-- Fila 3: Fecha de exhumación y Fecha de vencimiento -->
             <div class="fila">
                 <div class="form-group">
-                    <label>Contribuyente que acncela:</label>
-                    <input type="text" maxlength="50" autocomplete="off" class="form-control" name="contrcancela[]">
+                    <label>Contribuyente que cancela:</label>
+                    <input type="text" maxlength="50" autocomplete="off" class="form-control" id="contrcancela">
                 </div>
                 <div class="form-group">
                     <label>DUI:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="dui[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="dui">
                 </div>
                 <div class="form-group">
                     <label>Direccion:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="direccion[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="direccion">
                 </div>
                 <div class="form-group">
                     <label>Telefono:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="telefono[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="telefono">
                 </div>
                 <div class="form-group">
                     <label>Periodo cancelado:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="periodocancelado[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="periodocancelado">
                 </div>
-
 
             </div>
 
             <!-- Fila 3: Fecha de exhumación y Fecha de vencimiento -->
             <div class="fila">
 
-
                 <div class="form-group">
                     <label>Costo sin el 5%:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="costosin[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="costosin">
                 </div>
                 <div class="form-group">
                     <label>Costo con el 5%:</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="costocon[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="costocon">
                 </div>
                 <div class="form-group">
                     <label>Recibo de ingreso(tesoreria):</label>
-                    <input type="text" maxlength="8" autocomplete="off" class="form-control" name="recibo[]">
+                    <input type="text" maxlength="8" autocomplete="off" class="form-control" id="recibo">
                 </div>
                 <div class="form-group">
                     <label>Fecha de ingreso (tesoreria):</label>
-                    <input type="date" maxlength="8" autocomplete="off" class="form-control" name="fechateso[]">
+                    <input type="date" maxlength="8" autocomplete="off" class="form-control" id="fechateso">
                 </div>
 
             </div>
-
-
-
 
         </div>
     </form>
